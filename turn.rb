9c777f3198934d1b11d086ac5f47b25680dc7022
Sean Sullivan <dev@seanmedia.ca>
2020-05-25 21:19:48 +0000

    Create Turn class

--- turn.rb
+++ turn.rb
@@ -1,4 +1,20 @@
 class Turn
-  def initialize
+  def initialize(player)
+    @player = player
+    @question = Question.new
+  end
+
+  def question
+    @question.text
+  end
+
+  def guess(input)
+    correct = @question.answer(input.to_i)
+
+    unless correct
+      @player.lives -= 1
+    end
+    
+    return correct
   end
 end
