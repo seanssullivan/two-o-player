66ced64b42e15abab882ae0503bb04f1340f2893
Sean Sullivan <dev@seanmedia.ca>
2020-05-25 21:10:01 +0000

    Create Question class

--- question.rb
+++ question.rb
@@ -1,4 +1,14 @@
 class Question
+  attr_reader(:text)
+
   def initialize
+    num1 = rand(10)
+    num2 = rand(10)
+    @text = "What does #{num1} plus #{num2} equal?"
+    @correct_answer = num1 + num2
   end
+
+  def answer(guess)
+    return guess == @correct_answer ? true : false
+  end 
 end
